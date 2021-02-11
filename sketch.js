02d44ff31b4ca0f9e5b89a4aeac4505fe95c6f0a
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-11 17:18:30 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+
+
+function preload(){
+	
+}
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+
+
+	Engine.run(engine);
+
+
+}
+
+function draw() {
+
+
+
+}
+
