fe52a9cea44e6344930303d2340ccf9e35fb1121
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-14 11:22:34 -0600

    Add files via upload

--- bin.js
+++ bin.js
@@ -1,65 +1,26 @@
-class dustbin
-{
-   constructor(x,y){
-    var options = {
-        isStatic: true
+class Bin {
+    constructor(x, y, width, height) {
+      var options = {
+           isStatic: true,
+          'restitution':0.2,
+          'friction':1.0,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
     }
-       this.x=x;
-       this.y=y;
-       this.dustbinWidth=200;
-       this.dustbinHeight=213;
-       this.wallThickness=20;
-      
-       this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true})
-       this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-      
-      
- 
-       this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
-      
-       World.add(world, this.bottomBody)
-       World.add(world, this.leftWallBody)
-       World.add(world, this.rightWallBody);
- 
-   }
-   display()
-   {
-           var posBottom=this.bottomBody.position;
-           var posLeft=this.leftWallBody.position;
-           var posRight=this.rightWallBody.position;
- 
-          
- 
-           push()
-           translate(posLeft.x, posLeft.y);
-           imageMode(CENTER)
-           strokeWeight(4);
-           angleMode(RADIANS)
-           fill(255)
-           rotate(this.angle)
-           rect(0,0,this.wallThickness, this.dustbinHeight);
-           pop()
- 
-           push()
-           translate(posRight.x, posRight.y);
-           imageMode(CENTER)
-           strokeWeight(4);
-           angleMode(RADIANS)
-           fill(255)
-           rotate(-1*this.angle)
-           rect(0,0,this.wallThickness, this.dustbinHeight);
-           pop()
- 
-           push()
-           translate(posBottom.x, posBottom.y+10);
-           imageMode(CENTER)
-           strokeWeight(4);
-           angleMode(RADIANS)
-           fill(255)
-           rect(0,0,this.dustbinWidth, this.wallThickness);
-           pop()
-           image(this.image, 0, 0, this.dustbinWidth, this.dustbinHeight);
-          
-   }
- 
-}
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("yellow");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  }
